a7c76f2ddfa8f5c264c4858be33c786eb0f5f947
koutyuke <gunji.kousuke08@gmail.com>
2023-05-16 21:18:20 +0900

    ⭐(create)/drinkDataの作成

---
+++ Caffe-Mana/Consts/drinkDataBase.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+
+let drinkData: [DrinkData] = [
+    DrinkData(id: 1, name: "モンスターエナジー", cafeine: 142, price: 200, volume: 355, fileName: "monsterEnergy"),
+    DrinkData(id: 2, name: "モンスターエナジーラージ", cafeine: 200, price: 275, volume: 500, fileName: "monsterEnergy(large)"),
+    DrinkData(id: 3, name: "スーパーコーラ", cafeine: 142, price: 196, volume: 355, fileName: "monster-supercola"),
+    DrinkData(id: 4, name: "リザーブウォーターメロン", cafeine: 142, price: 180, volume: 355, fileName: "monster-watermelon"),
+    DrinkData(id: 5, name: "カオス", cafeine: 142, price: 190, volume: 355, fileName: "monster-chaos"),
+    DrinkData(id: 6, name: "パイプラインパンチ", cafeine: 142, price: 200, volume: 355, fileName: "monster-pipelinepunch"),
+    DrinkData(id: 7, name: "ロッシ", cafeine: 124, price: 200, volume: 355, fileName: "monster-rossi"),
+    DrinkData(id: 8, name: "マンゴーロコ", cafeine: 142, price: 200, volume: 355, fileName: "monster-mango"),
+    DrinkData(id: 9, name: "ウルトラ", cafeine: 140, price: 200, volume: 355, fileName: "monster-ultra"),
+    DrinkData(id: 10, name: "ウルトラパラダイス", cafeine: 142, price: 200, volume: 355, fileName: "monster-ultraparadaise"),
+    DrinkData(id: 11, name: "アブソリュートゼロ", cafeine: 142, price: 200, volume: 355, fileName: "monster-absolutezero"),
+    DrinkData(id: 12, name: "M3", cafeine: 140, price: 190, volume: 150, fileName: "monster-M3"),
+]
