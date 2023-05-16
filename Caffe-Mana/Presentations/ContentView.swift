10afc192c7d75337980f06e23cba706b36101a1c
koutyuke <gunji.kousuke08@gmail.com>
2023-05-16 21:20:15 +0900

    ⭐(create)/svgを表示するためのviewの作成

--- Caffe-Mana/Presentations/ContentView.swift
+++ Caffe-Mana/Presentations/ContentView.swift
@@ -7,6 +7,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            SVGImage(name: "monster-mango").frame(width:100,height:240);
         }
         .padding()
     }
