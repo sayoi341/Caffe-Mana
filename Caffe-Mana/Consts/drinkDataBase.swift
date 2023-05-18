c87b49cdb8b15b3af968ed52f7d5fbbf4283d584
koutyuke <gunji.kousuke08@gmail.com>
2023-05-18 21:52:54 +0900

    ➕(add)/redbull,zoneのdrinkDataの追加

--- Caffe-Mana/Consts/drinkDataBase.swift
+++ Caffe-Mana/Consts/drinkDataBase.swift
@@ -3,7 +3,7 @@ import Foundation
 
 let drinkData: [DrinkData] = [
     DrinkData(id: 1, name: "モンスターエナジー", cafeine: 142, price: 200, volume: 355, fileName: "monsterEnergy"),
-    DrinkData(id: 2, name: "モンスターエナジーラージ", cafeine: 200, price: 275, volume: 500, fileName: "monsterEnergy(large)"),
+    DrinkData(id: 2, name: "モンスターエナジー(500)", cafeine: 200, price: 275, volume: 500, fileName: "monsterEnergy-large"),
     DrinkData(id: 3, name: "スーパーコーラ", cafeine: 142, price: 196, volume: 355, fileName: "monster-supercola"),
     DrinkData(id: 4, name: "リザーブウォーターメロン", cafeine: 142, price: 180, volume: 355, fileName: "monster-watermelon"),
     DrinkData(id: 5, name: "カオス", cafeine: 142, price: 190, volume: 355, fileName: "monster-chaos"),
@@ -14,4 +14,15 @@ let drinkData: [DrinkData] = [
     DrinkData(id: 10, name: "ウルトラパラダイス", cafeine: 142, price: 200, volume: 355, fileName: "monster-ultraparadaise"),
     DrinkData(id: 11, name: "アブソリュートゼロ", cafeine: 142, price: 200, volume: 355, fileName: "monster-absolutezero"),
     DrinkData(id: 12, name: "M3", cafeine: 140, price: 190, volume: 150, fileName: "monster-M3"),
+    DrinkData(id: 13, name: "レッドブル(255)", cafeine: 80, price: 250, volume: 355, fileName: "redbull"),
+    DrinkData(id: 14, name: "レッドブル(355)", cafeine: 114, price: 250, volume: 355, fileName: "redbull"),
+    DrinkData(id: 15, name: "レッドブル(473)", cafeine: 151, price: 332, volume: 473, fileName: "redbull"),
+    DrinkData(id: 16, name: "レッドエディション", cafeine: 80, price: 133, volume: 250, fileName: "redbull-red"),
+    DrinkData(id: 17, name: "ブルーエディション", cafeine: 80, price: 150, volume: 250, fileName: "redbull-blue"),
+    DrinkData(id: 18, name: "パープルエディション", cafeine: 80, price: 187, volume: 250, fileName: "redbull-purple"),
+    DrinkData(id: 19, name: "イエローエディション", cafeine: 80, price: 175, volume: 250, fileName: "redbull-yello"),
+    DrinkData(id: 20, name: "シュガーフリー", cafeine: 80, price: 169, volume: 250, fileName: "redbull-sugarfree"),
+    DrinkData(id: 21, name: "ゾーン", cafeine: 150, price: 186, volume: 500, fileName: "zone"),
+    DrinkData(id: 22, name: "ユートピア", cafeine: 75, price: 190, volume: 500, fileName: "zone-utopia"),
+    DrinkData(id: 23, name: "ダイブ", cafeine: 75, price: 185, volume: 500, fileName: "zone-dive")
 ]
