328fac7b44d19b670f5c8dfdb743bda4ecbd21c3
sayoi341 <67829283+sayoi341@users.noreply.github.com>
2023-05-20 02:38:34 +0900

    ➕(add)/GameSceneの作成

--- Caffe-Mana/Presentations/Home.swift
+++ Caffe-Mana/Presentations/Home.swift
@@ -1,30 +1,71 @@
 import SwiftUI
 import RealmSwift
+import SpriteKit
+import ComposableArchitecture
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        self.backgroundColor = .clear
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            makeDrink(at: location)
+            
+        }
+    }
+    
+    func makeDrink(at position: CGPoint) {
+        let drink = SKSpriteNode(imageNamed: "monsterEnergy")
+        drink.physicsBody = SKPhysicsBody(rectangleOf: drink.size)
+        drink.position = position
+        addChild(drink)
+    }
+}
+
+
 
 struct Home: View {
     let year: Int
     @ObservedRealmObject var monthLog: MonthLogRecord
     
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.scaleMode = .resizeFill
+        return scene
+    }
     
     var body: some View {
-        VStack{
-            Text(String(year))
-            Text(String(monthLog.month))
-            
-            ForEach(monthLog.drinkLogs) { drinkLog in
-                HStack{
-                    Text(drinkLog.drinkId)
-                    Text(drinkLog._id)
-                }
-            }
-        }
+        SpriteView(scene: self.scene, options: [.allowsTransparency])
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        let monthLogs = MonthLogRecord(month: 1, DrinkLogs: List<DrinkLog>())
-        
-        Home(year: 1, monthLog: monthLogs)
+        Layout(
+            store: Store(
+                initialState: Root.State(),
+                reducer: Root()
+            ),
+            home: {
+                SideScrollCenter(isShowIndex: true) {
+                    Home(year: 2023, monthLog: MonthLogRecord(month: 4, DrinkLogs: List<DrinkLogRecord>()))
+                    Home(year: 2023, monthLog: MonthLogRecord(month: 5, DrinkLogs: List<DrinkLogRecord>()))
+                }
+                
+            },
+            add: {
+                SideScrollCenter(isShowIndex: false) {
+                    ForEach(0..<3) { index in
+                        Text("\(index)")
+                    }
+                }
+            },
+            setting: {
+                Text("setting")
+            }
+        )
     }
 }
